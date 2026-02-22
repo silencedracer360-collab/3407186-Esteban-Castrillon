3a953daf643b87b2e2f4b64c3ae75fdcd47406bf
silencedracer360-collab <silencedracer360@gmail.com>
2026-02-21 20:45:28 -0500

    Semana-02

--- week-04/3-proyecto/starter/models/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Models Barrel Export
- * Re-exports all models from a single entry point
- */
-
-// TODO: Import and re-export all models
-// export { default as Product } from './Product.js';
-// export { default as Category } from './Category.js';
--- week-04/3-proyecto/starter/ui/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * UI Barrel Export
- * Re-exports all UI modules from a single entry point
- */
-
-// TODO: Re-export all UI modules
-// export * from './render.js';
-// export * from './events.js';
